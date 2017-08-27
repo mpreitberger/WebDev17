7459759909159e5db1c5d8e8f5576984de3b00a3
mpreitberger <michael.p.reitberger@gmail.com>
2017-08-27 17:24:39 +0200

    FizzBuzz 1.0

---
+++ 8.2 FizzBuzz/FizzBuzz.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+print "-" * 20
+print "-" * 20
+print "FizzBuzz"
+print "-" * 20
+print "-" * 20
+
+x = int(raw_input("Please enter a number between 1 and 100:" ))
+print x
+
+try:
+    x = int(x)
+
+    for num in range(1, 100):
+        if num % 3 == 0:
+            print "Fizz"
+        elif num % 5 == 0:
+            print "Buzz"
+        elif num % 3 == 0 and num % 5 == 0:
+            print "FizzBuzz"
+        else:
+            print num
+
+except ValueError:
+    print "You have to enter a valid number."
